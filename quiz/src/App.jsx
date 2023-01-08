073cb822946291b5cdcd422a2d28da63fcf64953
chagasleandro <leandro_srs20@yahoo.com.br>
2023-01-08 20:19:53 -0300

    modify quiz/src/App.css and  quiz/src/components/Welcome.css

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,9 +1,12 @@
+import Welcome from './components/Welcome';
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-        <h1>Quiz App</h1>
+        <h1>Quiz de Programação</h1>
+        <Welcome />
     </div>
   )
 }
